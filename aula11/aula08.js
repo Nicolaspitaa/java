cd16a265a20937ba85987e31e332227e05cd1cec
Nicolas Pita <nicolaspitar@gmail.com>
2025-04-10 14:10:29 -0300

    Att curso de js

---
+++ aula11/aula08.js
@@ -0,0 +1,7 @@
+var vel = 60  
+
+console.log(` A velocidde do seu carro é de ${vel}km/h`)
+if (vel > 60){
+    console.log(`Voce ultrapassou o limite de velocidade permitida.  MULTADO!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'eua'
+
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'BR'){
+    console.log('Brasileiro')
+     
+} else{
+    console.log('Gringo')
+}
