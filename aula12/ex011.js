f242d068ae8341041b517ab734063f7e8a71a5ca
Nicolas Pita <nicolaspitar@gmail.com>
2025-04-11 08:55:56 -0300

    ex 12 js

---
+++ aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 11
+
+if (idade <16){
+    console.log('Nao vota')
+
+} else if (idade <18 || idade>=65) {
+    console.log('Voto Opcional')
+} else if (idade>=18){
+    console.log('VOTO OBRIGATORIO!')
+}
+
---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+
+var agora = new Date()
+var hora = agora.getTime
+
+console.log(`Agora são exatamente ${agora} horas.`)
+
+if(hora <12){
+    console.log('Bom dia!')
+} else if (hora <=18)
+    {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa noite!')
+}
