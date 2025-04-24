b445233645596b0af99699707fb8f48d9de5aeea
Nicolas Pita <nicolaspitar@gmail.com>
2025-04-24 13:26:57 -0300

    aulas de js

---
+++ JAVARAFA/exercicios/ex03.js
@@ -0,0 +1,15 @@
+function clicar(){
+ var nome = prompt('Digite Seu nome:')
+ var sn = prompt(`Olá ${nome}, Seja bem vindo! vamos prosseguir ?
+    1-SIM ou 2-NÃO`)
+    while(sn == '2'){
+        alert('OBRIGADO POR PARTICIPAR!')
+    } if (sn == '1'){
+        var iniciar = prompt('Que legal que vamos continuar !')
+    }
+
+
+
+
+    
+}
