ab59fe56ee77588651f5a350349019ab625a2877
Nicolas Pita <nicolaspitar@gmail.com>
2025-04-28 15:43:22 -0300

    gerador de senhas ex

---
+++ GERADOR/script.js
@@ -0,0 +1,11 @@
+function getcharttypes(){
+    var upper = document.getElementById('maiscula').checked
+    var lower = document.getElementById('minuscula').checked
+    var number = document.getElementById('numero').checked
+    var special = document.getElementById('especial').checked
+    
+
+
+}
+
+document.getElementById('gerar').addEventListener('click', function)
