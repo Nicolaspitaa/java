49ee4d0e61df844ce304e54ef08c23a138d4e950
Nicolas Pita <nicolaspitar@gmail.com>
2025-05-22 11:16:51 -0300

    exs

---
+++ ex-01/indes.js
@@ -0,0 +1,25 @@
+
+function soma(valor1, valor2){
+return valor1 + valor2;
+
+}
+
+
+function realParaDolar (real, cotacaodolar){
+
+    return real * cotacaodolar
+}
+
+
+var valorreal = 7.89;
+var cotacao = 5.60;
+
+
+
+var total = realParaDolar (valorreal, cotacao)
+
+alert("O valor do real é R$: "+ valorreal +"O valor em dolar é U$: "+total)
+
+function alertahello(){
+    alert("Ola Pessoal");
+}
